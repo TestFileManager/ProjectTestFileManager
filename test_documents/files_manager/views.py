4652f71293be2b61a02f41647dda0d770e714ed7
THIEF <Xakxer@tut.by>
2021-03-15 15:32:45 +0300

    Башка кипит ну вроде чет есть

--- test_documents/files_manager/views.py
+++ test_documents/files_manager/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+# -------------выгрузка файлов----------------------------------------
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+from .forms import PostForm
+from .models import Post
+
+
+# -----------------------------------------------------
 # Create your views here.
 
 
@@ -25,3 +34,18 @@ def in_work(request):
 def outdated(request):
     # подключаем HTML шаблон для главной страницы
     return render(request, 'files_manager/outdated.html')
+
+
+# ---- для выгрузки файлов ---------------------------
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "new_files.html"
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "post.html"
+    success_url = reverse_lazy("new_files.html")
+# -----------------------------------------------------
