84dcd79976dc1a58e57cd440ff522aac6956233d
THIEF <Xakxer@tut.by>
2021-04-13 17:25:42 +0300

    Непрошло и месяца как доделал перемещение файлов по разным странницам и добавил кнопки стоят по дурацки ну как есть

--- test_documents/files_manager/models.py
+++ test_documents/files_manager/models.py
@@ -2,20 +2,29 @@ from django.db import models
 
 # Create your models here.
 # ------ модель для загрузки файлов ----------
+
+
 class Post(models.Model):
-    comment = models.CharField(max_length= 200)
+    comment = models.CharField(max_length=200)
     upload = models.FileField(upload_to="file_manager_upload/")
+# ----------- Move --------------------------------
+    stage = models.CharField(max_length=200, default="new_files")
 
-#-------- Delete file ----------------------------
-    def delete(self,*args,**kwargs):
-        self.upload.delete()
-        super().delete(*args,**kwargs)
+    def get_absolute_url(self):
+        return '/new_files'
+# -------- Delete file ----------------------------
+
+def delete(self, *args, **kwargs):
+    self.upload.delete()
+    super().delete(*args, **kwargs)
 # --------------------------------------------------
-    def __str__(self):
-        return self.comment
 
-    # Меняем название таблицы в Админ Панеле
-    class Meta:
-        verbose_name = 'Файл'
-        verbose_name_plural = 'Файлы'
+def __str__(self):
+    return self.comment
+
+
+# Меняем название таблицы в Админ Панеле
+class Meta:
+    verbose_name = 'Файл'
+    verbose_name_plural = 'Файлы'
 # ----------------------------------------------
