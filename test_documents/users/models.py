6c9ded17e772764cb1a6347d2244c30fe2aac662
Vitalii <vitaliy.hrebeniuk@gmail.com>
2021-05-27 08:47:24 +0300

    поДчинил форму регистрации

--- test_documents/users/models.py
+++ test_documents/users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    id = models.AutoField(primary_key=True, unique=True) # Идентификатор
-    username = models.CharField(max_length=50, unique=True) # Логин
-    password = models.CharField(max_length=50)# False
-    email = models.EmailField(max_length=100, unique=True) # Email
-    is_active = models.BooleanField(default=True) # Статус активации
-    is_staff = models.BooleanField(default=False) # Статус админа
+    id = models.AutoField(primary_key=True, unique=True)  # Идентификатор
+    username = models.CharField(max_length=50, unique=True)  # Логин
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)  # Email
+    is_active = models.BooleanField(default=True)  # Статус активации
+    is_staff = models.BooleanField(default=False)  # Статус админа
 
     # Метод для отображения в админ панели
     def __str__(self):
-        return self.email
+        return self.username
